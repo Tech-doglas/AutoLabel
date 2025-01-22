dd30652f1b88e2c616841109da5b32ac2dd0c7f4
tech <tech.doglas@gmail.com>
2025-01-22 09:52:36 -0500

    update: chnaged HP template, added hp model

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 from flask import Flask, render_template, request, send_file
-import io
-from io import BytesIO
 from PIL import Image, ImageDraw, ImageFont
 import math
 import re
-from flask_cors import CORS  # Import the CORS function
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -14,6 +12,8 @@ CORS(app)  # Enable CORS for all routes
 # Define the base directory and the path to the data folder
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_FOLDER = os.path.join(BASE_DIR, 'data')
+HP_SPECIAL_MODEL = os.path.join(BASE_DIR, r'data\HP_special\model.txt')
+HP_Template = os.path.join(BASE_DIR, r'data\HP_special\HP Template.txt')
 
 # Image configurations for different models
 IMAGE_CONFIGS = {
@@ -24,33 +24,9 @@ IMAGE_CONFIGS = {
         'font_size': 14
     },
     'hp': {
-        'font_size': 6.5,
+        'font_size': 9.5,
     }
 }
-
-HP_keywords = ["HP Laptop", "Victus by HP Gaming Laptop"]
-
-SSD_value = [
-    "128GB",
-    "256GB",
-    "512GB",
-    "1TB",
-    "2TB"
-]
-
-RAM_value = [
-    "4GB",
-    "8GB",
-    "12GB",
-    "16GB",
-    "20GB",
-    "24GB",
-    "32GB",
-    "36GB",
-    "40GB",
-    "48GB",
-    "64GB",
-]
 
 @app.route('/')
 def index():
@@ -66,6 +42,19 @@ def generate_image():
     print(selected_text_files)
 
     base_name = re.sub(r'(_\d+)?\.txt$', '', selected_text_files)  # Remove suffix like _1 or _2
+
+    brand = selected_text_files.split()[0].lower()
+
+    special_HP_flag = False
+
+    if brand == 'hp':
+        with open(HP_SPECIAL_MODEL, 'r') as file:
+            model_content = file.read()
+            lines = model_content.splitlines()
+            for line in lines:
+                if line == base_name:
+                    special_HP_flag = True
+
     related_files = [
         f for f in os.listdir(DATA_FOLDER)
         if re.match(rf'{re.escape(base_name)}(_\d+)?\.txt$', f, re.IGNORECASE)
@@ -73,6 +62,7 @@ def generate_image():
 
     if not related_files:
         return "No related files found", 404
+    
 
     # Create an image for each related file
     image_urls = []  # Store URLs of generated images
@@ -85,20 +75,19 @@ def generate_image():
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
 
-        # Determine brand and configuration
-        brand = selected_text_files.split()[0].lower()
+        # Determine configuration
         formatConfigs = brand if brand in ["dell", "hp"] else "default"
         config = IMAGE_CONFIGS[formatConfigs]
 
         # Create an image in memory
-        image = Image.new("RGB", (1100, 480), "white")
+        width, height = 1100 , 480
+        image = Image.new("RGB", (width, height), "white")
         draw = ImageDraw.Draw(image)
 
         # Load fonts
         try:
             bold_font = ImageFont.truetype("arialbd.ttf", math.floor(config['font_size'] * (600 // 108)))
             regular_font = ImageFont.truetype("arial.ttf", math.floor(config['font_size'] * (600 // 108)))
-            title_font = ImageFont.truetype("arialbd.ttf", math.floor( 10.5 * (600 // 108)))
         except IOError:
             bold_font = regular_font = ImageFont.load_default()
 
@@ -107,41 +96,30 @@ def generate_image():
         x, y = 30, 50
         line_spacing = 10
 
-        for i, line in enumerate(lines):
-            # Bold and larger for specific headings
-            if brand == "dell":
-                font = bold_font if line.strip() in ["Performance", "Software"] else regular_font
-            elif brand == "hp":
-                # Check if the current line contains "HP Laptop" or "Victus by HP Gaming Laptop"
-                if any(keyword in line for keyword in HP_keywords):
-                    font = title_font
-                else:
-                    font = regular_font
-            else:
+        for line in lines:
+
+            match = re.search(r"\[(\w+)\]", line)
+            
+            if match and match.group(1) == "bold":  # Check if the word is 'bold'
+                # Remove everything after [bold] (including it)
                 font = bold_font
-                
-            if re.search(r'\b(SSD|Storage|STORAGE)\b', line, re.IGNORECASE):
-                    if brand in ["msi", "dell"]:
-                        line = re.sub(r"(\d+(?:GB|TB) SSD)", lambda m: f"{selected_ssd} SSD", line, flags=re.IGNORECASE)
-                    elif brand in ["acer", "lenovo"]:
-                        line = "Storage: " + selected_ssd
-                    else:
-                        if re.search(r'\b(SSD)\b', line, re.IGNORECASE):
-                            line = re.sub(r"(SSD.:\s*)(\S+)", lambda m: m.group(1) + selected_ssd, line, flags=re.IGNORECASE)
-                        else:
-                            line = re.sub(r"(STORAGE:\s*)(\S+)", lambda m: m.group(1) + selected_ssd, line, flags=re.IGNORECASE)
-            if re.search(r'\b(Memory|Ram)\b', line, re.IGNORECASE):
-                    if brand == "lenovo":
-                        line = "RAM: " + selected_ram
-                    elif brand == "asus":
-                        line = "• RAM: " + selected_ram
-                    elif brand == "acer":
-                        line = re.sub(r"(Memory:\s*\d+GB)\s*.*", lambda m: f"Memory: {selected_ram}", line, flags=re.IGNORECASE)
-                        line = re.sub(r"(Mémoire:\s*\d+GB)\s*.*", lambda m: f"Mémoire: {selected_ram}", line, flags=re.IGNORECASE)
-                    elif brand == "dell":
-                        line = re.sub(r"\d+GB Memory",  lambda m: f"{selected_ram} Memory", line, flags=re.IGNORECASE)
-            if brand == "msi":
-                    line = re.sub(r"(DDR[45]) \d+GB(?:\(\d+GB\*\d+\))?",  lambda m: f"{selected_ram} RAM", line, flags=re.IGNORECASE)
+                line = re.sub(r"\[bold\]", "", line).strip()
+                if brand == 'hp':
+                    bbox = draw.textbbox((0, 0), line, font=font)
+                    text_width = bbox[2] - bbox[0]  # bbox[2] is the right x-coordinate, bbox[0] is the left
+                    x = (width - text_width) / 2  # Center text horizontally
+                else:
+                    x = 30
+            else:
+                font = regular_font
+                x = 30
+            
+            
+            if match and match.group(1) == "SSD":
+                line = re.sub(r"\[SSD\]", selected_ssd, line)
+            elif match and match.group(1) == "RAM":
+                line = re.sub(r"\[RAM\]", selected_ram, line)
+
 
             # Draw text and calculate height using textbbox
             draw.text((x, y), line, fill="black", font=font)
@@ -154,8 +132,15 @@ def generate_image():
         image.save(img_path, format="PNG")
         image_urls.append(f"/static/{img_name}")
 
+    if special_HP_flag:
+        with open(HP_Template, 'r') as file:
+            HP_content = file.read()
+            lines = HP_content.splitlines()
+            for line in lines:
+                print(line)
+
+    
     # Serve the list of generated images in the HTML
-    # return render_template("display_image.html", image_urls=image_urls)
     return {"image_urls": image_urls}
 
 
@@ -167,4 +152,4 @@ def serve_image(filename):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080, ssl_context=("cert.pem", "key.pem")) #For server
-    # app.run(debug=True) # For testing 
+    # app.run(debug=True) # For testing
